4012cd149b367f7ee1ffa0bcc61060bbab1444b7
mintgreenmonkey <j.m.ward13+mintgreenmonkey@gmail.com>
2017-06-28 20:41:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## wdjs-metapod
+
+basic experimenting with wdjs using async/await. uses babel at the moment because it was less of a headache
+
+i will update this readme when there’s more to talk about
---
+++ test/specs/theinternet.js
@@ -0,0 +1,23 @@
+let expect = require('chai').expect
+const {Builder, By, promise, until} = require('selenium-webdriver');
+
+
+describe('https://the-internet.herokuapp.com/', function() {
+    var driver;
+    this.timeout(60000);
+
+    beforeEach(async function(){
+        driver = await new Builder().forBrowser('chrome').build();
+    });
+
+        after(async function() {
+        await driver.quit();
+    });
+
+    it('should have a h1 title that says \'Welcome to the Internet\'', async function() {
+        await driver.get('https://the-internet.herokuapp.com/');
+        let title = await driver.findElement(By.css('#content h1')).getText();
+        
+        expect(title).to.equal('Welcome to the Internet');
+    });
+});
